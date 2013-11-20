f9f4c803083257bf445e20deacf89e5f8573ee34
gryphius <oli@fuglu.org>
2013-11-20 21:26:18 +0100

    some lightshow demo fun

--- demo/lightshow/lightshow-eng.py
+++ demo/lightshow/lightshow-eng.py
@@ -2,60 +2,272 @@
 """The emperors new groove"""
 
 from lightshow import LightShow
+from colors import *
 import sys
+import random
+import os
 
 show=LightShow()
+show.audiodelay_offset=0
+
 for _ in range(6):
     show.add_light()
 
-show.soundfile='/home/gryphius/perfectworld.ogg'
-#colors
-red=(255,0,0)
-blue=(0,0,255)
-green=(0,255,0)
-yellow=(0,255,255)
-white=(255,255,255)
+show.soundfile=os.path.expanduser('~/perfectworld.ogg')
+
+# --- ALIAS SETUP --- #
 
 #lights
-left=[0,1,2]
-right=[3,4,5]
-all=[0,1,2,3,4,5]
+L1=0
+L2=1
+L3=2
+L4=3
+L5=4
+L6=5
+
+#custom colors
+green=android_green
+
+def choruslight(light,pos):
+    switchtime=1000.0
+    offset_per_light=switchtime*2/6
+    
+    #strophe 1
+    color1=spring_green
+    color2=light_sky_blue
+    
+    #strophe2
+    if pos>35839:
+        color1=cyan
+        color2=hot_magenta
+    
+    #bridge
+    if pos>54127:
+        color1=green_crayola
+        color2=han_blue
+    
+    #pitch change1
+    if pos>86316:
+        color1=electric_violet
+        color2=deep_sky_blue
+        
+    #pitch change2
+    if pos>90700:
+        color1=violet
+        color2=cg_red
+    
+    diff_r=color2[0]-color1[0]
+    diff_g=color2[1]-color1[1]
+    diff_b=color2[2]-color1[2]
+    step_r=diff_r/switchtime
+    step_g=diff_g/switchtime
+    step_b=diff_b/switchtime
+    abspos=(pos+light*offset_per_light)%(switchtime*2)
+    if abspos>switchtime:
+        abspos=switchtime*2-abspos
+    
+    
+    new_r=color1[0]+abspos*step_r
+    new_g=color1[1]+abspos*step_g
+    new_b=color1[2]+abspos*step_b
+        
+    return int(new_r),int(new_g),int(new_b)
+
+#lights
+left=[L1,L2,L3]
+right=[L4,L5,L6]
+all=[L1,L2,L3,L4,L5,L6]
 
 # ---------------------------------------- START
+print "Intro..."
 #piano intro
 show.set_color(1000,all,white,200)
-show.set_color(1200,[0,1,2],(0,0,80),8000)
-show.set_color(1200,[3,4,5],(0,80,0),8000)
+start=1200
+dur=2000
+for i in range(4):
+    st=int(start+i*dur/2)
+    show.transition(st,[L1,L3,L5],aquamarine,bittersweet,dur)
+    show.transition(st,[L2,L4,L6],bittersweet,aquamarine,dur)
+    st=st+int(dur/2)
+    show.transition(st,[L1,L3,L5],bittersweet,aquamarine,dur)
+    show.transition(st,[L2,L4,L6],aquamarine,bittersweet,dur)
 
 #trumpet start
-ts=9800
-show.set_color(ts+0,[3,4,5],red,1500)
-show.set_color(ts+700,0,blue,1000)
-show.set_color(ts+800,1,blue,1000)
-show.set_color(ts+900,2,blue,1000)
+show.set_color(8600,all,white,2000) #00:08 
+show.set_color(8808,L5,dark_orchid,1000) #00:08 
+show.set_color(8859,L4,dark_orchid,1000) #00:08 
+show.set_color(8993,L3,dark_orchid,1000) #00:08 
+show.set_color(9215,L2,dark_orchid,1000) #00:09 
 
-show.set_color(11400,all,yellow,1000)
-show.set_color(11813,all,green,1000)
-show.set_color(12096,all,yellow,1000)
-show.set_color(12299,all,green,1000)
-show.set_color(12420,all,yellow,1000)
-show.set_color(12779,all,green,1000)
-show.set_color(13000,all,yellow,1000)
+#blackout before hit
+show.set_color(9607,all,black,1000) #00:09 
+
+
+show.set_color(9838,all,white,1500) #00:09
+show.set_color(10380,L2,neon_fuchsia,500) #00:10 
+show.set_color(10480,L4,neon_fuchsia,500) #00:10 
+show.set_color(10600,L6,neon_fuchsia,300) #00:10 
+
+duration=1000
+show.set_color(11417,all,bright_pink,duration) #00:11 
+show.set_color(11736,all,bright_turquoise,duration) #00:11 
+show.set_color(11974,all,bright_pink,duration) #00:11 
+show.set_color(12222,all,bright_turquoise,duration) #00:12 
+show.set_color(12300,all,bright_pink,duration) #00:12 
+show.set_color(12664,all,bright_turquoise,duration) #00:12 
+show.set_color(12926,all,bright_pink,duration) #00:12 
+
+
+#random colors
+for pos in range(13770,15369): #00:13-00:15
+    randomcolor=(random.randint(0,255),random.randint(0,255),random.randint(0,255))
+    show.set_color(pos,all,randomcolor,1)
+    
 
 #off hits
-show.set_color(15617,all,white,200)
-show.set_color(16562,all,white,200)
+show.set_color(15617,all,chrome_yellow,200)
+show.set_color(16462,all,daffodil,200)
 
-show.set_color(17060,all,blue,200)
-show.set_color(17302,all,red,200)
-show.set_color(17391,all,blue,200)
+#triole
+show.set_color(17100,[L1,L2],ultra_pink,600)
+show.set_color(17202,[L3,L4],tiffany_blue,500)
+show.set_color(17291,[L5,L6],veronica,300)
 
-show.set_color(18043,all,white,200)
+show.set_color(17995,all,scarlet,200)
+
+
+print "chorus 1..."
+#00:19 strophe1
+for i in range(120000):
+    pos=19000+i
+    for light in all:
+        color=choruslight(light, pos)
+        show.set_color(pos,light,color,1)
+
+#hits am ende
+show.set_color(32573,all,lemon_lime,200) #00:32 
+show.set_color(33838,all,cadmium_orange,200) #00:33 
+show.set_color(34249,all,white,800) #00:34 
+show.set_color(34349,L1,red,100) #00:34
+show.set_color(34399,L2,red,100) #00:34
+show.set_color(34449,L3,red,100) #00:34
+show.set_color(34499,L4,red,100) #00:34
+show.set_color(34549,L5,red,100) #00:34
+show.set_color(34599,L6,red,100) #00:34
+
+
+show.set_color(34700,all,black,2100) #00:34 
+
+print "chorus 2..."
+## 00:38 strophe2 ##
+
+
+#triplehit
+show.set_color(52523,[L1,L6],school_bus_yellow,600) #00:52 
+show.set_color(52670,[L2,L5],screamin_green,400) #00:52 
+show.set_color(52794,[L3,L4],shocking_pink,200) #00:52
+
+show.set_color(53833,all,wild_strawberry,1000) #00:53 
+show.set_color(54032,[L2,L3],tractor_red,800) #00:54 
+show.set_color(54380,[L3,L4],yellow,600) #00:54 
+show.set_color(54594,[L4,L5],vivid_violet,400) #00:54 
+
+print "bridge..."
+#00:55 bridge
+
+show.set_color(59138,all,white,200) #00:59 
+
+
+
+show.set_color(61550,[L1,L2],red,100) #01:01 
+show.set_color(61716,[L3,L4],red,100) #01:01 
+show.set_color(61803,[L5,L6],red,100) #01:01 
+
+show.set_color(62064,[L5,L6],harlequin,100) #01:02 
+show.set_color(62196,[L3,L4],harlequin,100) #01:02 
+show.set_color(62335,[L1,L2],harlequin,100) #01:02 
+
+show.set_color(62476,all,banana_yellow,100) #01:02 
+show.set_color(62831,all,red,100) #01:02 
+show.set_color(63065,all,banana_yellow,100) #01:03 
+
+#prepare break
+show.set_color(68151,[L2,L3,L4,L5],bright_green,1000) #01:08 
+show.set_color(69310,[L2,L3,L4,L5],aureolin,1000) #01:09 
+
+#break
+show.set_color(70706,all,magenta,200) #01:10 
+show.set_color(71702,all,mango_tango,200) #01:11 
+
+print "refrain..."
+#01:12 refrain (whats his name...)
+show.set_color(72727,all,red,1000) #01:12 
+
+show.set_color(73200,[L1,L2],black,1000) #01:12 
+show.set_color(73350,[L3,L4],black,1200) #01:12 
+show.set_color(73500,[L5,L6],black,1300) #01:12 
+
+
+show.set_color(81176,[L1,L6],yellow,1000) #01:21                                                                                                                                                               
+show.set_color(81266,[L2,L5],yellow,1000) #01:21                                                                                                                                                               
+show.set_color(81475,[L3,L4],yellow,1000) #01:21                                                                                                                                                               
+show.set_color(81567,all,yellow,1000) #01:21   
+
+
+print "pitch shift..."
+#01:31 tonartwechsel
+
+#sax
+show.set_color(97439,all,black,2000) #01:37 
+color=bright_pink
+
+#right
+start=97500
+
+offset=int(350.0/6.0)
+show.set_color(start+0*offset,L1,color,100) #01:37 
+show.set_color(start+1*offset,L2,color,100) #01:37 
+show.set_color(start+2*offset,L3,color,100) #01:37
+show.set_color(start+3*offset,L4,color,100) #01:37
+show.set_color(start+4*offset,L5,color,100) #01:37
+show.set_color(start+5*offset,L6,color,100) #01:37
+
+#left
+color=dark_orange
+show.set_color(start+6*offset,L6,color,100) #01:37 
+show.set_color(start+7*offset,L5,color,100) #01:37 
+show.set_color(start+8*offset,L4,color,100) #01:37
+show.set_color(start+9*offset,L3,color,100) #01:37
+show.set_color(start+10*offset,L2,color,100) #01:37
+show.set_color(start+11*offset,L1,color,100) #01:37
+
+
+#hits after sax
+show.set_color(98385,all,medium_aquamarine,200) #01:38 
+show.set_color(98700,L4,color,100) #01:37
+show.set_color(98800,L3,color,100) #01:37
+show.set_color(98946,all,carmine_red,200) #01:38 
+
+
+
+#what's his name -hit hit
+show.set_color(104156,all,deep_sky_blue,2000) #01:44 
+show.set_color(105260,all,daffodil,2000) #01:45 
+show.set_color(106715,all,white,200) #01:46 
+show.set_color(107627,all,white,200) #01:47 
+show.set_color(107800,all,black,500) #01:47
+show.transition(108671,all,red,bittersweet_shimmer,500) #01:48
+
+#end
+show.set_color(133000,all,black,10000) #02:13 
+show.set_color(133274,all,magenta,200) #02:13 
+show.set_color(134444,all,harlequin,200) #02:14 
+show.set_color(134939,all,indigo,200) #02:14 
+show.set_color(135520,[L1,L3,L5],red,1000) #02:15 
+show.set_color(135520,[L2,L4,L6],white,1000) #02:15
 
 #---------------- GO ! -------------------------------
-
-show.debug=True
-show.debug_all_ts=True
+print "pygame startup..."
 
 offset=0
 end=None
--- demo/lightshow/lightshow.py
+++ demo/lightshow/lightshow.py
@@ -1,11 +1,115 @@
 import traceback
 import time
 import pygame
-import curses
 import sys
+import os
 sys.path.insert(0,'../../')
 
 from pycyborg import get_all_cyborgs
+
+class PGdisplay(object) :
+    screen = None;
+
+    def __init__(self):
+        "Ininitializes a new pygame screen using the framebuffer"
+        # Based on "Python GUI in Linux frame buffer"
+        # http://www.karoltomala.com/blog/?p=679
+        disp_no = os.getenv("DISPLAY")
+        if disp_no:
+            print "I'm running under X display = {0}".format(disp_no)
+
+        # Check which frame buffer drivers are available
+        # Start with fbcon since directfb hangs with composite output
+        drivers = ['fbcon', 'directfb', 'svgalib','x11']
+        found = False
+        for driver in drivers:
+            # Make sure that SDL_VIDEODRIVER is set
+            if not os.getenv('SDL_VIDEODRIVER'):
+                os.putenv('SDL_VIDEODRIVER', driver)
+            try:
+                pygame.display.init()
+            except pygame.error:
+                #print 'Driver: {0} failed.'.format(driver)
+                continue
+            found = True
+            break
+
+        if not found:
+            raise Exception('No suitable video driver found!')
+
+        size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
+        #print "Framebuffer size: %d x %d" % (size[0], size[1])
+        if driver=='x11':
+            size = (1024,768)
+            self.screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+        else:
+            self.screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
+        # Clear the screen to start
+        self.screen.fill((0, 0, 0))
+        # Initialise font support
+        pygame.font.init()
+        # Render the screen
+        pygame.display.update()
+
+    def __del__(self):
+        "Destructor to make sure pygame shuts down, etc."
+
+    def update(self,text,lightstates):
+        background = pygame.Surface(self.screen.get_size())
+        background = background.convert()
+        
+        #white background
+        background.fill((255,255,255))
+        
+        #position
+        font = pygame.font.Font(None, 30)
+        lineoffset=0
+        for line in text.split("\n"):
+            rtext = font.render(line, 1, (10, 10, 10))
+            trec=rtext.get_rect()
+            textpos = trec
+            textpos.y = 20+lineoffset
+            lineoffset+=25
+            background.blit(rtext, textpos)
+        
+        
+        #lights
+        hoffset=165
+        hstart=100
+        vpos=400
+        radius=80
+        texthoffset=30
+        textvoffset=20
+        for i in range(len(lightstates)):
+            state=lightstates[i]
+            hpos=hstart + i*hoffset
+            tpl=state.rgbtuple()
+            rtext = font.render(str(tpl), 1, (10, 10, 10))
+            trec=rtext.get_rect()
+            textpos = trec
+            textpos.x=hpos-radius+texthoffset
+            textpos.y=vpos+radius+textvoffset
+            background.blit(rtext,textpos)
+            border=0
+            #fill
+            try:
+                pygame.draw.circle(background,tpl,(hpos,vpos),radius,border)
+            except TypeError:
+                print "got onvalid color: %s"%(str(tpl))
+                
+            #border
+            pygame.draw.circle(background,(0,0,0),(hpos,vpos),radius,2)
+
+        # Blit everything to the screen
+        self.screen.blit(background, (0, 0))
+        pygame.display.flip()
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type== pygame.KEYDOWN:
+                return event.key
+            
+
+
 
 class LightState(object):
     def __init__(self,r,g,b):
@@ -34,6 +138,10 @@ class LightShow(object):
         self.debug_all_ts=False
         self.debug_lightsoff=False
         
+        #use this to offset the audiodelay on slow devices
+        self.audiodelay_offset=0
+        
+        
         self.soundfile=None
     
     def get_num_lights(self):
@@ -49,7 +157,24 @@ class LightShow(object):
         for light in lights:
             for d in range(duration):
                 self.states[light][ts+d]=LightState(color[0],color[1],color[2])
-    
+
+    def transition(self,ts,lights,startcolor,endcolor,duration):
+        """Transition from current color to another. duration and updatetime in secs"""
+        steps=duration
+        r,g,b=endcolor
+        start_r,start_g,start_b=startcolor
+        step_r=(r-start_r)/float(steps)
+        step_g=(g-start_g)/float(steps)
+        step_b=(b-start_b)/float(steps)
+        for step in range(int(steps)):
+            new_r=int(start_r+(step*step_r))
+            new_g=int(start_g+(step*step_g))
+            new_b=int(start_b+(step*step_b))
+            if new_r<0 or new_g<0 or new_b<0:
+                print new_r,new_g,new_b
+            for light in lights:
+                self.states[light][ts+step]=LightState(new_r,new_g,new_b)
+
     def get_state(self,ts,light):
         try:
             res=self.states[light][ts]
@@ -66,22 +191,19 @@ class LightShow(object):
     def play(self,offset=0,end=None):
         trb=None
         pygame.mixer.init()
-        pygame.mixer.music.load(self.soundfile)
+        
+        #load track with sound to get total length
+        soundfile=self.soundfile
+        totalpos=int(pygame.mixer.Sound(soundfile).get_length()*1000)
+        totalpos_str=self._ms_to_minsec(totalpos)
+        pygame.mixer.music.load(soundfile)
         secoffset=int(offset/1000.0)
         pygame.mixer.music.play(0,secoffset)
         ticktime=0.001
         lightsoff=LightState(0,0,0)
                 
-        stdscr = curses.initscr()
-        curses.noecho()
-        curses.cbreak()
-        stdscr.border(0)
-        stdscr.keypad(1)
-        stdscr.nodelay(1)
-        
-        maxposcount=25
-        poswin=curses.newwin(maxposcount+1,20,3,90)
-        poswin.border(0)
+
+        display=PGdisplay()
         
         
         savedpositions=[]
@@ -99,36 +221,23 @@ class LightShow(object):
                 if end!=None and ts>=end:
                     break
                 
-                stdscr.addstr(1,2, "Position: %s  %s"%(ts,self._ms_to_minsec(ts)))
-                stdscr.addstr(2,2, "Cyborgs detected: %s Used: %s"%(num_cyborgs,self.get_num_lights()))
+                states=[]
                 for light in range(self.get_num_lights()):
-                    state=self.get_state(ts, light)
-                    strstate=str(state)
+                    state=self.get_state(ts-self.audiodelay_offset, light)
                     if state==None:
                         state=lightsoff
-                        strstate=""                    
-                    stdscr.addstr(10+light,2, "Light %s: %s                    "%(light,strstate))
+                    states.append(state)
+                    
                     
                     if light<num_cyborgs:
                         cyborgs[light].set_rgb_color(state.r,state.g,state.b)
-                        
-                        
-                    
-                stdscr.refresh()
                 
-                #keypress
-                c=stdscr.getch()
-                    
-                if c==ord('c'):
+                message="Track: %s msec=%s time=%s\n"%(soundfile,totalpos,totalpos_str)     
+                message+="Position: %s\n"%(ts)
+                message+="Time: %s\n"%(self._ms_to_minsec(ts))
+                key=display.update(message, states)
+                if key==pygame.K_c:
                     savedpositions.append(ts)
-                
-                poswin.addstr(1,1,"'c':record pos")
-                disp=savedpositions[-maxposcount:]
-                for i in range(len(disp)):
-                    curpos=disp[i]
-                    poswin.addstr(2+i,1,str(curpos))
-                poswin.refresh()
-                
                 time.sleep(ticktime)
         except KeyboardInterrupt:
             pass
@@ -137,26 +246,16 @@ class LightShow(object):
         for cyborg in cyborgs:
             cyborg.lights_off()
         
-        curses.nocbreak(); stdscr.keypad(0); curses.echo()
-        curses.endwin()
-        if trb==None:
-            print "clean shutdown"
-        else:
+        if trb!=None:
             print trb
         
         if len(savedpositions)>0:
             print "Saved positions: %s"%savedpositions
+            for s in savedpositions:
+                print """show.set_color(%s,lightlist,color,duration) #%s """%(s,self._ms_to_minsec(s))
     
 if __name__=='__main__':
-    show=LightShow()
-    print show._ms_to_minsec(85000)
-    for _ in range(2):
-        show.add_light()
-    
-    show.set_color(0, 0, (255,0,0), 100)
-    show.set_color(101, [0,1],(0,0,255), 100)
-    
-    assert show.get_state(50, 0).rgbtuple()==(255,0,0)
+    pass
     
                 
     
